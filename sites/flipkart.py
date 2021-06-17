25533c8939a677ea8b40271c9d98e77c1ea50bee
Kiran-Sawant <kbsawant1005@gmail.com>
2021-06-17 12:36:47 +0530

    functions to extract data from flipkart.com

---
+++ sites/flipkart.py
@@ -0,0 +1,11 @@
+import asyncio
+
+async def get_all_absolute_links(product:str):
+
+    url = "https://www.flipkart.com/search?q={}&page={}"
+    
+    return f"{product} nahi hai!"
+
+async def run_flipkart(product:str):
+
+    pass
